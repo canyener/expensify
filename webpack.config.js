a7eaa60f8cccc9b08c1fb13778169723e68d3d01
canadaslar <can@adaslar.co.uk>
2019-12-15 00:41:53 +0300

    Installs and configures webpack dev server and serves content in index.html

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+}
