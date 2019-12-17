73274c7c4dc7aa891a0fc96f0c31279299cf8798
canyener <can.yener@hotmail.com.tr>
2019-12-17 21:29:58 +0300

    Imports Switch from react-router and implements it to routes constant with a 404 route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
@@ -29,14 +29,21 @@ const HelpPage = () => (
   </div>
 )
 
+const NotFoundPage = () => (
+  <div>
+    404! 
+  </div>
+)
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage}/>
       <Route path="/help" component={HelpPage}/>
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 )
 
