ce89583651df9ec541069d8c183ea845ad9ae103
canyener <can.yener@hotmail.com.tr>
2019-12-17 22:34:12 +0300

    Moves components into their own seperate files

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExportDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+)
+
+export default ExportDashboardPage
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,45 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from my dashboard component
-  </div>
-)
-
-const AddExpensePage = () => (
-  <div>
-    This is from my add expense component
-  </div>
-)
-
-const EditExpensePage = () => (
-  <div>
-    This is from my edit expense component
-  </div>
-)
-
-const HelpPage = () => (
-  <div>
-    This is from my help page
-  </div>
-)
-
-const NotFoundPage = () => (
-  <div>
-    404! - <Link to="/">Go Home</Link>
-  </div>
-)
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
-)
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
+import AddExpensePage from '../components/AddExpensePage'
+import EditExpensePage from '../components/EditExpensePage'
+import HelpPage from '../components/HelpPage'
+import NotFoundPage from '../components/NotFoundPage'
+import Header from '../components/Header'
 
 const AppRouter = () => (
   <BrowserRouter>
