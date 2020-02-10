550ebc1f5bae38212cc07152280a10f74022fb7a
canyener <can.yener@hotmail.com.tr>
2020-02-10 22:40:08 +0300

    Adds action generator function for INCREMENT action

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,11 +1,17 @@
 import { createStore } from 'redux'
 
+//Action generators - functions that return action objects
+
+const incrementCount = (payload = {}) => ({ 
+  type: 'INCREMENT',
+  incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+})
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       }
     case 'DECREMENT':
       const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
@@ -29,16 +35,16 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState())
 })
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-})
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// })
+
+store.dispatch(incrementCount({ incrementBy: 5 }))
 
 //unsubscribe()
 
-store.dispatch({
-  type: 'INCREMENT'
-})
+store.dispatch(incrementCount())
 
 store.dispatch({
   type: 'RESET'
