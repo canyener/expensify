9499011217d0493cdb3ce18b4ec8ea5edfdab599
canyener <canyener1337@gmail.com>
2020-02-18 21:33:13 +0300

    Defines counterReducer

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -21,7 +21,11 @@ const resetCount = () => ({
   type: 'RESET'
 })
 
-const store = createStore((state = { count: 0 }, action) => {
+//Reducers
+//1. Reducers are pure functions
+//2. Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
@@ -42,7 +46,9 @@ const store = createStore((state = { count: 0 }, action) => {
     default: 
       return state
   }
-})
+}
+
+const store = createStore(countReducer)
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState())
