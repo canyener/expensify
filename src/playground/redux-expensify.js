4c599e2a084a9b40a8859766dbe602557e89f029
canyener <canyener1337@gmail.com>
2020-02-18 22:22:01 +0300

    Adds expensesReducer and registers it to createStore method

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,35 @@
 import { createStore, combineReducers } from 'redux'
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+//Expenses Reducer
+
+const expensesReducerDefaultState = []
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+// Store creation
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer
+  })
+)
+
+console.log(store.getState())
+
 const demoState = {
   expenses: [{
     id: '123qweqwe123',
