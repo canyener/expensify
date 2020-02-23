57966a49a5a373c4cf34c32eef667e913392fe25
canyener <canyener1337@gmail.com>
2020-02-23 20:53:32 +0300

    Sets webpack config entry back to app.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/playground/redux-expensify.js',
+  entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
