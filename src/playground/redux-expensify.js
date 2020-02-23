f6f09409b1a30a3ba892d2d69ad549d3b7fed9d3
canyener <canyener1337@gmail.com>
2020-02-23 19:20:04 +0300

    Adds sorting by date

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -134,6 +134,10 @@ const getVisibleExpenses= (expenses,  { text, sortBy, startDate, endDate }) => {
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
 
     return startDateMatch && endDateMatch && textMatch
+  }).sort((a, b) => {
+    if(sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1
+    }
   })
 }
 
@@ -152,13 +156,13 @@ store.subscribe(() => {
   console.log(visibleExpenses)
 })
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }))
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }))
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }))
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
 
-store.dispatch(setTextFilter('rent'))
+// store.dispatch(setTextFilter('rent'))
 // store.dispatch(setTextFilter())
 
 // store.dispatch(sortByAmount())
