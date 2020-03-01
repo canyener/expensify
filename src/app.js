6da0c7fe705910eb973c86871f3662da8564f889
canyener <canyener1337@gmail.com>
2020-03-01 22:48:00 +0300

    Adds a setTimeout function to demonstrate that component re-renders when store is changed

--- src/app.js
+++ src/app.js
@@ -20,6 +20,10 @@ store.dispatch(addExpense({ description: 'Gas Bill' }))
 
 store.dispatch(setTextFilter('water'))
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'))
+}, 3000)
+
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 
