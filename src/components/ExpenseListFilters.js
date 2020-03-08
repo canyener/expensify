c1103c25996fbf1a7edfd8e7ee2e2ef3846ab952
canyener <canyener1337@gmail.com>
2020-03-08 14:00:15 +0300

    Adds onChange handler to input

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { setTextFilter } from '../actions/filters'
+
 const ExpenseListFilters = (props) => (
   <div>
-    <input type="text" value={props.filters.text} />
+    <input type="text" value={props.filters.text} onChange={(e) =>{
+      props.dispatch(setTextFilter(e.target.value))
+    }} />
   </div>
 )
 
