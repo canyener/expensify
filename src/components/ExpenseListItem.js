0b2d047ee103e739e7d3abe17b212ada03c2ba7d
canyener <canyener1337@gmail.com>
2020-03-08 14:08:06 +0300

    Adds onClick handler to remove expenses

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const ExpenseListItem  = ({ description, amount, createdAt }) => (
+import { removeExpense } from '../actions/expenses' 
+
+const ExpenseListItem  = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }))
+    }}>Remove</button>
   </div>
 )
 
-export default ExpenseListItem
+export default connect()(ExpenseListItem)
