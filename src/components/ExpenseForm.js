c25ee699948a36a5e0581a29600b390bea2f8516
canyener <canyener1337@gmail.com>
2020-03-09 00:14:46 +0300

    Fixes regex pattern so that we cannot start the amount with .

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -23,7 +23,7 @@ export default class ExpenseForm extends React.Component {
   onAmountChange = (e) => {
     const amount = e.target.value;
 
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   }
