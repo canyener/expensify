363110d9fdb67730a5456c274aa967b13fc9dc3b
canyener <canyener1337@gmail.com>
2020-03-11 23:08:28 +0300

    Adds Link for description

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { removeExpense } from '../actions/expenses' 
 
 const ExpenseListItem  = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>{amount} - {createdAt}</p>
     <button onClick={() => {
       dispatch(removeExpense({ id }))
