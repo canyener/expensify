0acbfadf9e3eea786daf3963ef72f841234860a5
canyener <canyener1337@gmail.com>
2020-03-11 23:17:48 +0300

    Adds props to ExpenseForm component

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import ExpenseForm from './ExpenseForm'
+
 const EditExpensePage = (props) => {
-  console.log(props)
   return (
     <div>
-      Editing the expense with id of {props.match.params.id}
+      <ExpenseForm
+        expense={props.expense} 
+        onSubmit={(expense) => {
+          console.log('updated', expense)
+        }} 
+      />
     </div>
   )
 }
