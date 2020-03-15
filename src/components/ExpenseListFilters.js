f6b296f6b637fc4a1da708c00b9a25466fd1c8eb
canyener <canyener1337@gmail.com>
2020-03-15 17:04:09 +0300

    Changes component to class based one

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,33 +1,65 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { DateRangePicker } from 'react-dates'
+
 import { setTextFilter, sortByDate, sortByAmount } from '../actions/filters'
 
-const ExpenseListFilters = (props) => (
-  <div>
-    <input 
-      type="text" 
-      value={props.filters.text} 
-      onChange={(e) =>{
-        props.dispatch(setTextFilter(e.target.value))
-      }} 
-    />
-    <select 
-      value={props.filters.sortBy} 
-      onChange={(e) => {
-        const sortBy = e.target.value
-        if (sortBy === 'date') {
-          props.dispatch(sortByDate())
-        } else if (sortBy === 'amount') {
-          props.dispatch(sortByAmount())
-        }
-      }}
-    >
-      <option value="date">Date</option>
-      <option value="amount">Amount</option>
-    </select>
-  </div>
-)
+class ExpenseListFilters extends React.Component {
+  render() {
+    return (
+      <div>
+        <input 
+          type="text" 
+          value={this.props.filters.text} 
+          onChange={(e) =>{
+            this.props.dispatch(setTextFilter(e.target.value))
+          }} 
+        />
+        <select 
+          value={this.props.filters.sortBy} 
+          onChange={(e) => {
+            const sortBy = e.target.value
+            if (sortBy === 'date') {
+              this.props.dispatch(sortByDate())
+            } else if (sortBy === 'amount') {
+              this.props.dispatch(sortByAmount())
+            }
+          }}
+        >
+          <option value="date">Date</option>
+          <option value="amount">Amount</option>
+        </select>
+      </div>
+    )
+  }
+}
+
+// const ExpenseListFilters = (props) => (
+//   <div>
+//     <input 
+//       type="text" 
+//       value={props.filters.text} 
+//       onChange={(e) =>{
+//         props.dispatch(setTextFilter(e.target.value))
+//       }} 
+//     />
+//     <select 
+//       value={props.filters.sortBy} 
+//       onChange={(e) => {
+//         const sortBy = e.target.value
+//         if (sortBy === 'date') {
+//           props.dispatch(sortByDate())
+//         } else if (sortBy === 'amount') {
+//           props.dispatch(sortByAmount())
+//         }
+//       }}
+//     >
+//       <option value="date">Date</option>
+//       <option value="amount">Amount</option>
+//     </select>
+//   </div>
+// )
 
 const mapStateToProps = (state) => {
   return {
