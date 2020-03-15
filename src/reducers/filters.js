7d181fbbbe8080a3b8ee05d86cc9d94d335a9c12
canyener <canyener1337@gmail.com>
2020-03-15 16:43:24 +0300

    Sets start and end dates default value

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,10 +1,11 @@
-//Filters Reducer
+import moment from 'moment'
 
+//Filters Reducer
 const filtersReducerDefaultState = {
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 }
 
 export default (state = filtersReducerDefaultState, action) => {
