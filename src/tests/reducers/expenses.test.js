b0c3d2eaa7ce54e444532e5e3880d95b811a6a3f
canyener <can.yener@hotmail.com.tr>
2020-03-22 21:43:54 +0300

    Moves seed expenses data to its own fixture file and imports it to necesssary test files

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,4 +1,5 @@
 import expensesReducer from '../../reducers/expenses'
+import expenses from '../fixtures/expenses'
 
 test('Should set default state', () => {
   const state = expensesReducer(undefined, { type : '@@INIT' })
