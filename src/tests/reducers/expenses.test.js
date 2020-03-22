e577788604f451fb5f590bf4f92e3a4cd4165faa
canyener <can.yener@hotmail.com.tr>
2020-03-22 21:48:40 +0300

    Adds test cases for removing expense

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -5,3 +5,23 @@ test('Should set default state', () => {
   const state = expensesReducer(undefined, { type : '@@INIT' })
   expect(state).toEqual([])
 })
+
+test('Should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id // 2
+  }
+
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([expenses[0], expenses[2]])
+})
+
+test('Should NOT remove expense if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-99'
+  }
+
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual(expenses)
+})
