61b8204938c788d6a1f3c3f18a7617b6c1f1a2fb
canyener <can.yener@hotmail.com.tr>
2020-03-22 20:59:40 +0300

    Adds test case for default state setup

---
+++ src/tests/reducers/filters.test.js
@@ -0,0 +1,12 @@
+import moment from 'moment'
+import filtersReducer from '../../reducers/filters'
+
+test('Should setup default filter values', () => {
+  const state = filtersReducer(undefined, { type: '@@INIT' })
+  expect(state).toEqual({
+    text: '',
+    sortBy: 'date',
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')
+  })
+})
