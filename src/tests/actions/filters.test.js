f9cb7adb13bf31f981fa36fbb575673f1a75ebb3
canyener <can.yener@hotmail.com.tr>
2020-03-22 18:42:58 +0300

    Adds test cases for set text filter action generator

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,5 +1,12 @@
 import moment from 'moment'
-import { setStartDate, setEndDate, sortByAmount, sortByDate } from '../../actions/filters'
+
+import { 
+  setStartDate, 
+  setEndDate, 
+  sortByAmount, 
+  sortByDate, 
+  setTextFilter 
+} from '../../actions/filters'
 
 test('Should generate set start date action object', () => {
   const actualAction = setStartDate(moment(0))
@@ -25,4 +32,20 @@ test('Should generate sort by amount action object', () => {
 test('Should generate sort by date action object', () => {
   const actualAction = sortByDate()
   expect(actualAction).toEqual({ type: 'SORT_BY_DATE' })
+})
+
+test('Should generate set text filter action object with provided text value', () => {
+  const actualAction = setTextFilter('test string')
+  expect(actualAction).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: 'test string'
+  })
+})
+
+test('Should generate set text filter action object with default text value', () => {
+  const actualAction = setTextFilter()
+  expect(actualAction).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: ''
+  })
 })
