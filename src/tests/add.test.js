7727b2b42011995546426e3fa5f625ef2c3abd1a
canyener <can.yener@hotmail.com.tr>
2020-03-22 17:40:12 +0300

    Adds example test cases

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,4 +1,5 @@
 const add = (a, b) => a + b
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`
 
 test('Should add two numbers', () => {
   const result = add(3, 4)
@@ -7,4 +8,14 @@ test('Should add two numbers', () => {
   // if(result !== 7) {
   //   throw new Error(`You added 4 and. The result wass ${result}. Expected 7`)
   // }
+})
+
+test('Should generate greeting from name', () => {
+  const actual = generateGreeting('Cancan')
+  expect(actual).toBe('Hello Cancan!')
+})
+
+test('Should generate greeting for no name', () => {
+  const actual = generateGreeting()
+  expect(actual).toBe('Hello Anonymous!')
 })
