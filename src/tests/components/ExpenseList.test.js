4eabdcbaa2f935114349da8778dc8126116aa0a7
canyener <can.yener@hotmail.com.tr>
2020-03-23 18:05:54 +0300

    Adds test case for rendering with no expenses

--- src/tests/components/ExpenseList.test.js
+++ src/tests/components/ExpenseList.test.js
@@ -8,3 +8,8 @@ test('Should render ExpenseList with expenses', () => {
   const wrapper = shallow(<ExpenseList expenses={expenses} />)
   expect(wrapper).toMatchSnapshot()
 })
+
+test('Should render ExpenseList with empty message', () => {
+  const wrapper = shallow(<ExpenseList expenses={[]} />)
+  expect(wrapper).toMatchSnapshot()
+})
