c0d76580217f191ec7cd2325dba1bb4fd0134ca7
canyener <can.yener@hotmail.com.tr>
2020-03-23 17:17:53 +0300

    Sets up enzyme for react 16

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => (
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
   </header>
 )
 
