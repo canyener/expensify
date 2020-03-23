d627247ab8704a5af0d73df9f1a41fa8bc0cb788
canyener <can.yener@hotmail.com.tr>
2020-03-23 17:49:25 +0300

    Moves react-test-renderer example to playground folder

---
+++ src/playground/react-test-renderer-example.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactShallowRenderer from 'react-test-renderer/shallow'
+
+import Header from '../../components/Header'
+
+test('Should render Header correctly', () => {
+  const renderer = new ReactShallowRenderer()
+  
+  renderer.render(<Header />)
+  expect(renderer.getRenderOutput()).toMatchSnapshot()
+})
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-//import ReactShallowRenderer from 'react-test-renderer/shallow'
 
 import Header from '../../components/Header'
 
@@ -8,9 +7,4 @@ test('Should render Header correctly', () => {
   const wrapper = shallow(<Header />)
   
   expect(wrapper).toMatchSnapshot()
-  //expect(wrapper.find('h1').length).toBe(1)
-  //expect(wrapper.find('h1').text()).toBe('Expensify')
-  // const renderer = new ReactShallowRenderer()
-  // renderer.render(<Header />)
-  // expect(renderer.getRenderOutput()).toMatchSnapshot()
 })
