f087d84604adde5918672d47eeb1678143550000
canyener <canyener1337@gmail.com>
2020-03-24 18:31:44 +0300

    Adds mock file for moment and updates snapshot for ExpenseForm

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme'
 
 import ExpenseForm  from '../../components/ExpenseForm'
 
-test('Should render ExpenseForm correctly', () => {
+test('Should render ExpenseForm correctly with NO expense data', () => {
   const wrapper= shallow(<ExpenseForm />)
   expect(wrapper).toMatchSnapshot()
 })
