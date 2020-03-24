6ac3216e9b204c26d7b0ad0a9c9245052cc9ba9c
canyener <canyener1337@gmail.com>
2020-03-24 20:16:35 +0300

    Removes redundant console.log statement

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -55,7 +55,6 @@ export default class ExpenseForm extends React.Component {
         createdAt: this.state.createdAt.valueOf(),
         note: this.state.note
       })
-      console.log('submitted!')
     }
   }
   render() {
