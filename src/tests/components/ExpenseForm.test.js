ee833f94105834a85ad9d8a513ac890f2afcd9a1
canyener <canyener1337@gmail.com>
2020-03-24 19:02:29 +0300

    Adds testcase for submitting the form

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,14 @@ test('Should render ExpenseForm correctly with given expense data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[0]} />)
   expect(wrapper).toMatchSnapshot()
 })
+
+test('Should render error message for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm />)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  })
+  //expect(wrapper.state('error').length).toBeGreaterThan(0)
+  expect(wrapper.state('error')).toBe('Please provide description and amount.')
+  expect(wrapper).toMatchSnapshot()
+})
