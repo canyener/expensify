2f00ce6e3ea2bd613a14b3647c6cc68f3c8ee040
canyener <canyener1337@gmail.com>
2020-03-24 19:14:47 +0300

    Adds testcase for note change

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -16,10 +16,13 @@ test('Should render ExpenseForm correctly with given expense data', () => {
 
 test('Should render error message for invalid form submission', () => {
   const wrapper = shallow(<ExpenseForm />)
+
   expect(wrapper).toMatchSnapshot()
+  
   wrapper.find('form').simulate('submit', {
     preventDefault: () => {}
   })
+  
   //expect(wrapper.state('error').length).toBeGreaterThan(0)
   expect(wrapper.state('error')).toBe('Please provide description and amount.')
   expect(wrapper).toMatchSnapshot()
@@ -28,9 +31,21 @@ test('Should render error message for invalid form submission', () => {
 test('Should set description on input change', () => {
   const value = 'New description'
   const wrapper = shallow(<ExpenseForm />)
+  
   wrapper.find('input').at(0).simulate('change', {
     target: { value }
   })
 
   expect(wrapper.state('description')).toBe(value)
+})
+
+test('Should set note on textarea change', () => {
+  const expectedValue = 'New note'
+  const wrapper = shallow(<ExpenseForm />)
+
+  wrapper.find('textarea').at(0).simulate('change', {
+    target: { value: expectedValue }
+  })
+
+  expect(wrapper.state('note')).toBe(expectedValue)
 })
