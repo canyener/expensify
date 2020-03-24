ea9cdc869e6c2f6b68e6407165b8b1e5f162caa4
canyener <canyener1337@gmail.com>
2020-03-24 20:16:05 +0300

    Adds test case for onSubmit to have been called with correct data

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -70,3 +70,19 @@ test('Should NOT set amount if input value is invalid', () => {
 
   expect(wrapper.state('amount')).toBe('')
 })
+
+test('Should call onSubmit prop for valid form submission', () => {
+  const onSubmitSpy = jest.fn()
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />)
+
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  })
+  expect(wrapper.state('error')).toBe('')
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  })
+})
