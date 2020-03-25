d688c7c411356cdb388858d0a6cd4efbff02e1b1
canyener <canyener1337@gmail.com>
2020-03-25 20:21:20 +0300

    Removes example dispatch calls

--- src/app.js
+++ src/app.js
@@ -6,32 +6,11 @@ import AppRouter from './routers/AppRouter'
 
 import configureStore from './store/configureStore'
 
-import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
-
-import getVisibleExpenses from './selectors/expenses'
-
 import './styles/styles.scss'
 import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
 
-
 const store = configureStore()
-
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }))
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
-
-// store.dispatch(setTextFilter('water'))
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('bill'))
-// }, 3000)
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-
-console.log(visibleExpenses)
 
 const jsx = (
   <Provider store={store}>
