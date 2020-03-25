2f69cd9cf76036fe5e339c232aedc2996a5d542a
canyener <canyener1337@gmail.com>
2020-03-25 15:26:42 +0300

    Adds test case for onSubmit handling

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import { AddExpensePage } from '../../components/AddExpensePage'
+import expenses from '../fixtures/expenses'
 
 test('Should render AddExpensePage correctly', () => {
   const onSubmitSpy = jest.fn()
@@ -10,4 +11,16 @@ test('Should render AddExpensePage correctly', () => {
   const wrapper = shallow(<AddExpensePage onSubmit={onSubmitSpy} history={historySpy} />)
 
   expect(wrapper).toMatchSnapshot()
+})
+
+test('Should handle onSubmit', () => {
+  const onSubmitSpy = jest.fn()
+  const historySpy = { push: jest.fn() }
+
+  const wrapper = shallow(<AddExpensePage onSubmit={onSubmitSpy} history={historySpy} />)
+
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
+
+  expect(historySpy.push).toHaveBeenLastCalledWith('/')
+  expect(onSubmitSpy).toHaveBeenLastCalledWith(expenses[1])
 })
