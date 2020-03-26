ecb7b0861370347552d8e2c6ab29d73756c1050b
canyener <canyener1337@gmail.com>
2020-03-26 17:54:36 +0300

    Creates a basic express server for production

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const publicPath = path.join(__dirname, '..', 'public')
+
+app.use(express.static(publicPath))
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(3000, () => {
+  console.log('Server is up!')
+})
