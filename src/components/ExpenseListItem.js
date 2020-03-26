b08bca8e2bf4cb7a050dd7e2e3c5862c75c9854e
canyener <canyener1337@gmail.com>
2020-03-27 00:06:53 +0300

    Imports numeral and formats amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem  = ({ id, description, amount, createdAt }) => (
   <div>
@@ -8,7 +9,7 @@ const ExpenseListItem  = ({ id, description, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      {amount} 
+      {numeral(amount / 100).format('$0,0.00')} 
       - 
       {moment(createdAt).format('MMM Do, YYYY')}
     </p>
