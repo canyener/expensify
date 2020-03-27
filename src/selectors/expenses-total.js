cced5ae7b5b20877d679e7799013209d0228b95e
canyener <canyener1337@gmail.com>
2020-03-27 15:10:51 +0300

    Refactors selector function

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,9 +1,5 @@
 export default (expenses) => {
-  if (expenses.length === 0) {
-    return 0
-  } else {
-    return expenses
-      .map(expense => expense.amount)
-      .reduce((sum, value) => sum + value)
-  }
+  return expenses
+    .map(expense => expense.amount)
+    .reduce((sum, value) => sum + value, 0)
 }
