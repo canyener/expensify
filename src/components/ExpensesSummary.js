4783b2b92def87fe3c0da2f8a11f7186cb5dffd2
canyener <canyener1337@gmail.com>
2020-03-27 18:11:45 +0300

    Imports selectors and maps state to props

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 import numeral from 'numeral'
 
+import selectExpenses from '../selectors/expenses'
+import selectExpensesTotal from '../selectors/expenses-total'
+
 export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   const expenseWord = expenseCount === 1 ? 'expense': 'expenses'
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
@@ -11,3 +14,14 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     </div>
   )
 }
+
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters)
+
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses) 
+  }
+}
+
+export default connect(mapStateToProps)(ExpensesSummary)
