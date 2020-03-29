8827a677c158697015ca020f1d252689a8113190
canyener <can.yener@hotmail.com.tr>
2020-03-29 16:18:37 +0300

    Removes example calls

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,30 +16,16 @@ firebase.analytics()
 
 const database = firebase.database()
 
-database.ref().set({
-  name: 'Can Yener',
-  age: 35,
-  isSingle: true,
-  location: {
-    city: 'Istanbul',
-    country: 'Turkey'
-  }
-}).then(() => {
-  console.log('Data is saved')
-}).catch(e => {
-  console.log('This failed', e)
-})
-
-//database.ref().set('This is test data')
-
-// database.ref('age').set(36)
-// database.ref('location/city').set('Tekirdag')
-
-database.ref('attributes').set({
-  height: 187,
-  weight: 100
-}).then(() => {
-  console.log('Attributes added')
-}).catch(e => {
-  console.log('This failed', e)
-})
+// database.ref().set({
+//   name: 'Can Yener',
+//   age: 35,
+//   isSingle: true,
+//   location: {
+//     city: 'Istanbul',
+//     country: 'Turkey'
+//   }
+// }).then(() => {
+//   console.log('Data is saved')
+// }).catch(e => {
+//   console.log('This failed', e)
+// })
