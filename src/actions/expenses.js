4d1a0ef1f5babbfb81b9e951fee617275f15478e
canyener <can.yener@hotmail.com.tr>
2020-04-01 00:31:59 +0300

    Returns database push function to use promise chaining

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -18,7 +18,7 @@ export const startAddExpense = (expenseData = {}) => {
 
     const expense = { description, note, amount, createdAt }
 
-    database.ref('expenses').push(expense).then((ref) => {
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
