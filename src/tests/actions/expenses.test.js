6242135f1e30d1bbeacf431c68f4b09f67b37592
canyener <can.yener@hotmail.com.tr>
2020-04-01 00:52:18 +0300

    Adds test case for action to add expense to redux store and databasse

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,12 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
+
+import database from '../../firebase/firebase'
+
+const createMockStore = configureMockStore([thunk])
 
 test('Should setup remove expense action object', () => {
   const actualAction = removeExpense({ id: '123abc' })
@@ -27,6 +34,34 @@ test('Should setup add expense action object with provided values', () => {
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
     expense: expenses[2]
+  })
+})
+
+test('Should add expense to database and store', (done) => {
+  const store = createMockStore({})
+
+  const expenseData = {
+    description: 'Test desc',
+    amount: 3000,
+    note: 'Test note',
+    createdAt: 1000
+  }
+
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        ...expenseData
+      }
+    })
+
+    database.ref(`expenses/${actions[0].expense.id}`).once('value').then((snapshot) => {
+      
+      expect(snapshot.val()).toEqual(expenseData)
+      done()
+    })
   })
 })
 
