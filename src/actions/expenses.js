baa2d630aab8f759356b3e1a7b17b6a4e1b2a374
canyener <can.yener@hotmail.com.tr>
2020-03-31 23:56:41 +0300

    Adds startAddExpense action

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,23 +1,32 @@
 import {v4 as uuidv4 } from 'uuid'
+import database from '../firebase/firebase'
 
 // ADD_EXPENSE
-export const addExpense = (
-  { 
-    description = '', 
-    note = '', 
-    amount = 0, 
-    createdAt = 0 
-  } = {}
-) => ({
+export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
-  expense: {
-    id: uuidv4(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+  expense
 })
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = '', 
+      note = '', 
+      amount = 0, 
+      createdAt = 0 
+    } = expenseData
+
+    const expense = { description, note, amount, createdAt }
+
+    database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }))
+    })
+  }
+}
+
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
