06e81deb2088e631fe5a54c20f5a4107423c8f0e
canyener <can.yener@hotmail.com.tr>
2020-04-01 00:31:08 +0300

    Tweaks component tests around new async action

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -4,12 +4,12 @@ import { shallow } from 'enzyme'
 import { AddExpensePage } from '../../components/AddExpensePage'
 import expenses from '../fixtures/expenses'
 
-let addExpenseSpy, historySpy, wrapper
+let startAddExpenseSpy, historySpy, wrapper
 
 beforeEach(() => {
-  addExpenseSpy = jest.fn(),
+  startAddExpenseSpy = jest.fn(),
   historySpy = { push: jest.fn() }
-  wrapper = shallow(<AddExpensePage addExpense={addExpenseSpy} history={historySpy} />)
+  wrapper = shallow(<AddExpensePage startAddExpense={startAddExpenseSpy} history={historySpy} />)
 })
 
 test('Should render AddExpensePage correctly', () => {
@@ -18,7 +18,7 @@ test('Should render AddExpensePage correctly', () => {
 
 test('Should submit correct expense on onSubmit', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
-  expect(addExpenseSpy).toHaveBeenLastCalledWith(expenses[1])
+  expect(startAddExpenseSpy).toHaveBeenLastCalledWith(expenses[1])
 })
 
 test('Should redirect to / after submitting data', () => {
