4539520da858049f425cba5b5b5cd5fe5dc20c2d
canyener <can.yener@hotmail.com.tr>
2020-04-04 16:34:11 +0300

    Tweaks config object to read values from env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArvMh7NuM7UHOGtgG78A3Vy-QOQct_apU",
-  authDomain: "expensify-88855.firebaseapp.com",
-  databaseURL: "https://expensify-88855.firebaseio.com",
-  projectId: "expensify-88855",
-  storageBucket: "expensify-88855.appspot.com",
-  messagingSenderId: "1038160342450",
-  appId: "1:1038160342450:web:9ea9c36816f25733bbc8ab",
-  measurementId: "G-C3VCK9QMTK"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 firebase.initializeApp(firebaseConfig)
