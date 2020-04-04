8de3e5752f0b411fb42a5c53a4bf893328190cf0
canyener <canyener1337@gmail.com>
2020-04-05 01:51:57 +0300

    Adds env configuration file

---
+++ config/env.config.js
@@ -0,0 +1,8 @@
+const { config } = require('dotenv')
+process.env.NODE_ENV  = process.env.NODE_ENV || 'development'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else if (process.env.NODE_ENV === 'development') {
+  config({ path: '.env.development' })
+}
