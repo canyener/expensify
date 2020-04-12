3a261bfabb26d370fddbfdbc01982364a6a029fc
canyener <can.yener@hotmail.com.tr>
2020-04-12 21:16:50 +0300

    Changes firebase config properties with dummy ones

--- src/playground/firebase.js
+++ src/playground/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArvMh7NuM7UHOGtgG78A3Vy-QOQct_apU",
-  authDomain: "expensify-88855.firebaseapp.com",
-  databaseURL: "https://expensify-88855.firebaseio.com",
-  projectId: "expensify-88855",
+  apiKey: "firebase-api-key",
+  authDomain: "example-db.firebaseapp.com",
+  databaseURL: "https://example-db.firebaseio.com",
+  projectId: "example-project-id",
   storageBucket: "expensify-88855.appspot.com",
-  messagingSenderId: "1038160342450",
-  appId: "1:1038160342450:web:9ea9c36816f25733bbc8ab",
-  measurementId: "G-C3VCK9QMTK"
+  messagingSenderId: "example-messaging-id",
+  appId: "example-app-id",
+  measurementId: "example-measurement-id"
 }
 
 firebase.initializeApp(firebaseConfig)
