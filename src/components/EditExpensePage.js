beef8846efc8f66ca4c0d4e732dd22ded396f14c
canyener <can.yener@hotmail.com.tr>
2020-04-12 21:34:45 +0300

    Tweaks component to use startEditExpense async action

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { editExpense, startRemoveExpense } from '../actions/expenses'
+import { startEditExpense, startRemoveExpense } from '../actions/expenses'
 
 import ExpenseForm from './ExpenseForm'
 
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.editExpense(this.props.expense.id, expense)
+    this.props.startEditExpense(this.props.expense.id, expense)
     this.props.history.push('/')
   }
   onRemove =() => {
@@ -36,7 +36,7 @@ const mapStateToProps = (state, props) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
   startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 })
 
