b47ebfda3ef0a4e998a8ba85874790fe7e46e4e0
canyener <can.yener@hotmail.com.tr>
2020-04-12 21:59:04 +0300

    Refactors startEditExpense async action

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -50,14 +50,11 @@ export const editExpense = (id, updates) => ({
 
 export const startEditExpense = (id, updates) => {
   return (dispatch) => {
-    return database.ref(`expenses/${id}`).update({
-      ...updates
-    }).then(() => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
       dispatch(editExpense(id, updates))
     })
   }
 }
-
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
