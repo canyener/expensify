7d6467dd9a68870c9b361012225fda98360a6b17
canyener <can.yener@hotmail.com.tr>
2020-04-13 23:52:41 +0300

    Adds LoginPage component

---
+++ src/components/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class LoginPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Login</button>
+      </div>
+    )
+  }
+}
