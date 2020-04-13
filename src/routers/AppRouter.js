b0d94fd7bf82c53fb24ab1b7ecc6ab0b9a129257
canyener <can.yener@hotmail.com.tr>
2020-04-14 01:11:14 +0300

    Imports connected component (LoginPage)

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import { LoginPage } from '../components/LoginPage'
+import LoginPage from '../components/LoginPage'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
 import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
