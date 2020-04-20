cc90626de749f9dac5c339bb9dad6fa04428941b
canyener <can.yener@hotmail.com.tr>
2020-04-21 00:39:07 +0300

    Updates LoginPage snapshot

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -4,6 +4,6 @@ import { shallow } from 'enzyme'
 import { LoginPage } from '../../components/LoginPage'
 
 test('Should render LoginPage correctly', () => {
-  const wrapper = shallow(<LoginPage />)
+  const wrapper = shallow(<LoginPage startLogin={()=> {}} />)
   expect(wrapper).toMatchSnapshot()
 })
