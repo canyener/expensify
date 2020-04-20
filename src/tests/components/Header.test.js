2c1665a9d8c076d134134cf94b5d16b8d3d8e3df
canyener <can.yener@hotmail.com.tr>
2020-04-21 00:33:35 +0300

    Updates test case for Header

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import Header from '../../components/Header'
+import { Header } from '../../components/Header'
 
 test('Should render Header correctly', () => {
-  const wrapper = shallow(<Header />)
+  const wrapper = shallow(<Header startLogout={() => {}} />)
   
   expect(wrapper).toMatchSnapshot()
 })
