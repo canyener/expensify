7f0066d4d89f245c1d40b0fffa76ba78b1bc096a
canyener <can.yener@hotmail.com.tr>
2020-04-21 01:41:28 +0300

    Adds describe blocks

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -3,16 +3,24 @@ import { shallow } from 'enzyme'
 
 import { Header } from '../../components/Header'
 
-test('Should render Header correctly', () => {
-  const wrapper = shallow(<Header startLogout={() => {}} />)
-  
-  expect(wrapper).toMatchSnapshot()
+describe('Header', () => {
+  describe('#Snapshots', () => {
+    test('Should render Header correctly', () => {
+      const wrapper = shallow(<Header startLogout={() => {}} />)
+      
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('#Functionality', () => {
+    test('Should call startLogout on button click', () => {
+      const startLogoutSpy = jest.fn()
+      const wrapper = shallow(<Header startLogout={startLogoutSpy} />)
+    
+      wrapper.find('button').simulate('click')
+      expect(startLogoutSpy).toHaveBeenCalled()
+    })
+  })
 })
 
-test('Should call startLogout on button click', () => {
-  const startLogoutSpy = jest.fn()
-  const wrapper = shallow(<Header startLogout={startLogoutSpy} />)
 
-  wrapper.find('button').simulate('click')
-  expect(startLogoutSpy).toHaveBeenCalled()
-})
