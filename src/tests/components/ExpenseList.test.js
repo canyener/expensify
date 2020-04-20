b49471fdde95f27e6dcd9a545bc2fbc8071a34d4
canyener <can.yener@hotmail.com.tr>
2020-04-21 01:32:32 +0300

    Adds describe blocks

--- src/tests/components/ExpenseList.test.js
+++ src/tests/components/ExpenseList.test.js
@@ -4,12 +4,16 @@ import { shallow } from 'enzyme'
 import { ExpenseList } from '../../components/ExpenseList'
 import expenses from '../fixtures/expenses'
 
-test('Should render ExpenseList with expenses', () => {
-  const wrapper = shallow(<ExpenseList expenses={expenses} />)
-  expect(wrapper).toMatchSnapshot()
-})
-
-test('Should render ExpenseList with empty expense array', () => {
-  const wrapper = shallow(<ExpenseList expenses={[]} />)
-  expect(wrapper).toMatchSnapshot()
+describe('ExpenseList', () => {
+  describe('#Snapshots', () => {
+    test('Should render ExpenseList with expenses', () => {
+      const wrapper = shallow(<ExpenseList expenses={expenses} />)
+      expect(wrapper).toMatchSnapshot()
+    })
+    
+    test('Should render ExpenseList with empty expense array', () => {
+      const wrapper = shallow(<ExpenseList expenses={[]} />)
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
 })
