82c84ad7787aded5baf611272caeba95d63ce2a3
canyener <can.yener@hotmail.com.tr>
2020-04-21 01:17:11 +0300

    Adds describe blocks

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -20,28 +20,37 @@ beforeEach(() => {
   ) 
 })
 
-test('Should render EditExpensePage correctly', () => {
-  expect(wrapper).toMatchSnapshot()
-})
+describe('ExitExpensePage', () => {
+  describe('#Snapshots', () => {
+    test('Should render EditExpensePage correctly', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
 
-test('Should handle startEditExpense', () =>{
-  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2])
-  expect(startEditExpenseSpy).toHaveBeenLastCalledWith(expenses[2].id, expenses[2])
-})
-
-test('Should redirect to / on startEditExpense action', () => {
-  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2])
-  expect(historySpy.push).toHaveBeenLastCalledWith('/')
-})
-
-test('Should handle startRemoveExpense', () => {
-  wrapper.find('button').simulate('click')
-  expect(startRemoveExpenseSpy).toHaveBeenLastCalledWith({
-    id: expenses[2].id
+  describe('#Functionality', () => {
+    test('Should handle startEditExpense', () =>{
+      wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2])
+      expect(startEditExpenseSpy).toHaveBeenLastCalledWith(expenses[2].id, expenses[2])
+    })
+    
+    test('Should redirect to / on startEditExpense action', () => {
+      wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2])
+      expect(historySpy.push).toHaveBeenLastCalledWith('/')
+    })
+    
+    test('Should handle startRemoveExpense', () => {
+      wrapper.find('button').simulate('click')
+      expect(startRemoveExpenseSpy).toHaveBeenLastCalledWith({
+        id: expenses[2].id
+      })
+    })
+    
+    test('Should redirect to / on startRemoveExpense action', () => {
+      wrapper.find('button').simulate('click')
+      expect(historySpy.push).toHaveBeenLastCalledWith('/')
+    })
   })
 })
 
-test('Should redirect to / on startRemoveExpense action', () => {
-  wrapper.find('button').simulate('click')
-  expect(historySpy.push).toHaveBeenLastCalledWith('/')
-})
+
+
