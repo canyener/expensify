d1696328e2aff8480f85353893b4b8ae1cf3a322
canyener <can.yener@hotmail.com.tr>
2020-04-21 00:37:33 +0300

    Adds test case for logout button click

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -8,3 +8,11 @@ test('Should render Header correctly', () => {
   
   expect(wrapper).toMatchSnapshot()
 })
+
+test('Should call startLogout on button click', () => {
+  const startLogoutSpy = jest.fn()
+  const wrapper = shallow(<Header startLogout={startLogoutSpy} />)
+
+  wrapper.find('button').simulate('click')
+  expect(startLogoutSpy).toHaveBeenCalled()
+})
