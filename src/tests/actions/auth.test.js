4d8c8110fdda1465652706767a652c543a661a16
canyener <can.yener@hotmail.com.tr>
2020-04-23 22:18:35 +0300

    Refactors test cases

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -4,15 +4,18 @@ describe('auth actions', () => {
   describe('#LOGIN', () => {
     test('Should generate login action object', () => {
       const actualAction = login('1234qwe')
+
       expect(actualAction).toEqual({
         type: 'LOGIN',
         uid: '1234qwe'
       }) 
     })
   })
+
   describe('#LOGOUT', () => {
     test('Should generate logout action object', () => {
       const actualAction = logout()
+      
       expect(actualAction).toEqual({ type: 'LOGOUT' })
     })
   })
