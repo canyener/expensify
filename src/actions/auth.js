2e825391249408cae383a07f9710466482a06f99
canyener <can.yener@hotmail.com.tr>
2020-04-23 21:44:45 +0300

    Adds login and logout action generators

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,19 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase'
 
+export const login = (uid) => ({
+  type: 'LOGIN',
+  uid
+})
+
 export const startLogin = () => {
   return () => {
     return firebase.auth().signInWithPopup(googleAuthProvider)
   }
 }
+
+export const logout = () => ({
+  type: 'LOGOUT'
+})
 
 export const startLogout = () => {
   return () => {
