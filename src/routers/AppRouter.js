4428bfdf874adf179ad2b3e14563f47bb7f0269c
canyener <can.yener@hotmail.com.tr>
2020-05-03 16:37:18 +0300

    Removes Header and adds PrivateRoute

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,19 +9,18 @@ import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
 import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFoundPage'
-import Header from '../components/Header'
+import PrivateRoute from './PrivateRoute'
 
 export const history = createBrowserHistory()
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact={true} />
-        <Route path="/dashboard" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage}/>
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
         <Route path="/help" component={HelpPage}/>
         <Route component={NotFoundPage} />
       </Switch>
