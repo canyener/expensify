d7c9624c9bd6c5485de6cc215fbcbe7bd0f9210e
canyener <can.yener@hotmail.com.tr>
2020-05-03 20:53:11 +0300

    Restructures Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { startLogout } from '../actions/auth'
 
 export const Header = ({ startLogout }) => (
   <header>
-    <h1>Expensify</h1>
-    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <Link to="/dashboard">
+      <h1>Expensify</h1>
+    </Link>
     <button onClick={startLogout}>Logout</button>
   </header>
 )
