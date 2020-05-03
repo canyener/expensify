e017af6ca4d25907a6b618cfbe6dc050d088c61c
canyener <can.yener@hotmail.com.tr>
2020-05-03 18:00:54 +0300

    Adds user to startEditExpense and startRemoveExpense async actions

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -34,8 +34,9 @@ export const removeExpense = ({ id } = {}) => ({
 }) 
 
 export const startRemoveExpense = ({ id } = {}) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).remove().then(() => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid
+    return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
       dispatch(removeExpense({ id }))
     })
   }
@@ -49,8 +50,9 @@ export const editExpense = (id, updates) => ({
 })
 
 export const startEditExpense = (id, updates) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).update(updates).then(() => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid
+    return database.ref(`/users/${uid}/expenses/${id}`).update(updates).then(() => {
       dispatch(editExpense(id, updates))
     })
   }
@@ -63,8 +65,9 @@ export const setExpenses = (expenses) => ({
 })
 
 export const startSetExpenses = () => {
-  return (dispatch) => {
-    return database.ref('expenses').once('value').then(snapshot => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid
+    return database.ref(`users/${uid}/expenses`).once('value').then(snapshot => {
       const expenses = []
 
       snapshot.forEach(childSnapshot => {
