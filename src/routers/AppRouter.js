56ee2acf945d8a0e05b3b945fad9ec3ad9dafb7a
canyener <can.yener@hotmail.com.tr>
2020-05-03 17:27:22 +0300

    Uses PublicRoute for LoginPage component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,6 +9,7 @@ import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
 import NotFoundPage from '../components/NotFoundPage'
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 export const history = createBrowserHistory()
 
@@ -16,7 +17,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
