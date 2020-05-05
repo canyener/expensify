1883cbe9e9e5da43d5482e34d8d403442d25a3fd
canyener <can.yener@hotmail.com.tr>
2020-05-06 00:54:35 +0300

    Applies styles to EditExpensePage component

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -10,29 +10,32 @@ export class EditExpensePage extends React.Component {
     this.props.startEditExpense(this.props.expense.id, expense)
     this.props.history.push('/')
   }
-  onRemove =() => {
+  onRemove = () => {
     this.props.startRemoveExpense({ id: this.props.expense.id })
     this.props.history.push('/')
   }
   render() {
     return (
       <div>
-        <ExpenseForm
-          expense={this.props.expense} 
-          onSubmit={this.onSubmit} 
-        />
-        <button 
-          onClick={this.onRemove}
-        >
-          Remove
-        </button>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Edit Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm
+            expense={this.props.expense}
+            onSubmit={this.onSubmit}
+          />
+          <button className="button button--secondary" onClick={this.onRemove}>Remove Expense</button>
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state, props) => ({
-    expense: state.expenses.find(expense => expense.id === props.match.params.id)
+  expense: state.expenses.find(expense => expense.id === props.match.params.id)
 })
 
 const mapDispatchToProps = (dispatch) => ({
