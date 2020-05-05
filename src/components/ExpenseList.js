55b7427ef1e199613a66d9d43b87668b738580e0
canyener <can.yener@hotmail.com.tr>
2020-05-06 01:51:11 +0300

    Applies styles to ExpenseList component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,17 +5,26 @@ import ExpenseListItem from './ExpenseListItem'
 import selectExpenses from '../selectors/expenses'
 
 export const ExpenseList = (props) => (
-  <div>
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
     {
       props.expenses.length === 0 ? (
-        <p>No expenses</p>
+        <div className="list-item list-item--message">
+          <span>No expenses</span>
+        </div>
       ) : (
-        props.expenses.map(expense => <ExpenseListItem 
-          key={expense.id} 
-          {...expense}
-        />)
-      )
+          props.expenses.map(expense => <ExpenseListItem
+            key={expense.id}
+            {...expense}
+          />)
+        )
     }
+    </div>
   </div>
 )
 
